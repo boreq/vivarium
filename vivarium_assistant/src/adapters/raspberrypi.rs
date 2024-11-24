1d3d24e63b61a86644b705f3f661bbb808c2195d
boreq <boreq@0x46.net>
2024-11-24 15:15:14 +0100

    What

--- vivarium_assistant/src/adapters/raspberrypi.rs
+++ vivarium_assistant/src/adapters/raspberrypi.rs
@@ -43,3 +43,62 @@ impl domain::OutputPin for OutputPin {
         self.pin.set_high();
     }
 }
+
+
+pub struct PressureTemperatureSensor {
+    i2c:
+
+}
+
+impl  PressureTemperatureSensor {
+    pub fn new()  -> {
+
+    }
+
+    pub fn begin() {
+        if (isConnected() == false)
+        return false;
+
+    //Wait 40 ms after power-on before reading temp or humidity. Datasheet pg 8
+    delay(40);
+
+    //Check if the calibrated bit is set. If not, init the sensor.
+    if (isCalibrated() == false)
+    {
+        //Send 0xBE0800
+        initialize();
+
+        //Immediately trigger a measurement. Send 0xAC3300
+        triggerMeasurement();
+
+        delay(75); //Wait for measurement to complete
+
+        uint8_t counter = 0;
+        while (isBusy())
+        {
+            delay(1);
+            if (counter++ > 100)
+                return (false); //Give up after 100ms
+        }
+
+        //This calibration sequence is not completely proven. It's not clear how and when the cal bit clears
+        //This seems to work but it's not easily testable
+        if (isCalibrated() == false)
+        {
+            return (false);
+        }
+    }
+
+    //Check that the cal bit has been set
+    if (isCalibrated() == false)
+        return false;
+
+    //Mark all datums as fresh (not read before)
+    sensorQueried.temperature = true;
+    sensorQueried.humidity = true;
+
+    return true;
+    }
+
+
+}
